9f5d1a4a15e91438c60f12f1b01139868f311f70
Biswajeet Mandal <biswajeet163@gmail.com>
2021-03-30 19:34:02 +0530

    Initial commitss

---
+++ src/test/java/lectures/Lecture1.java
@@ -0,0 +1,45 @@
+package lectures;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import beans.Person;
+
+import com.google.common.collect.ImmutableList;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import mockdata.MockData;
+import org.assertj.core.util.Lists;
+import org.junit.Test;
+
+
+public class Lecture1 {
+
+  @Test
+  public void imperativeApproach() throws IOException {
+    List<Person> people = MockData.getPeople();
+    // 1. Find people aged less or equal 18
+    // 2. Then change implementation to find first 10 people
+
+    List<Person> peoples = people.stream()
+            .filter(p -> p.getAge() <= 18)
+            .limit(5)
+            .collect(Collectors.toList());
+
+    peoples.forEach(System.out::println);
+
+
+    people-> people.get
+
+
+
+  }
+
+  @Test
+  public void declarativeApproachUsingStreams() throws Exception {
+    ImmutableList<Person> people = MockData.getPeople();
+
+  }
+}
